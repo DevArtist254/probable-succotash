e0d2c6d2048737b172585f97ec2b2b0677a0f03b
Kevin wasonga <kevinwasonga415@outlook.com>
2025-04-29 19:03:02 +0300

    prod

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
 
 const app = express();
 const port = 3000;
 
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/dist/index.html"));
+});
 
 app.get("/app/v1/product", (req, res) => {
   res.json({
