af9e577ad0299f176f44a0ba44d1e28e552b4fa3
Kevin wasonga <kevinwasonga415@outlook.com>
2025-05-11 10:27:00 +0300

    bar

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,44 +2,12 @@
   <div>
     <div>
       <router-view></router-view>
-      <router-view name="NavBar"></router-view>
-      <!-- <SidebarNav /> -->
     </div>
   </div>
-
 </template>
 
 <script>
-import SidebarNav from './components/Dashboard/SidebarNav.vue';
-import NavBar from './components/NavBar.vue';
-
-export default {
-  components: {
-    NavBar,
-    SidebarNav
-  }
-}
+export default {};
 </script>
 
-<style>
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-} */
-
-/* nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-} */
-</style>
+<style></style>
