59d6ed51d0890d3e936280cd99e0a615714dda5f
Kevin wasonga <kevinwasonga415@outlook.com>
2025-05-13 02:36:39 +0300

    try-2

--- server/app.js
+++ server/app.js
@@ -43,21 +43,9 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-  res.setHeader(
+  rres.setHeader(
     "Content-Security-Policy",
-    [
-      "default-src 'self' 'unsafe-eval'",
-      "script-src 'self' 'unsafe-eval' https://unpkg.com",
-      "style-src 'self' 'unsafe-inline' https://unpkg.com https://fonts.googleapis.com",
-      "font-src 'self' https://fonts.gstatic.com",
-      "img-src 'self' data: https://* http://j0k400sc0k80gwwcs8kcgkow.devartist.art",
-      "script-src 'self' 'unsafe-eval' http://j0k400sc0k80gwwcs8kcgkow.devartist.art",
-      "style-src 'self' 'unsafe-inline' http://j0k400sc0k80gwwcs8kcgkow.devartist.art",
-      "connect-src 'self'",
-      "frame-src 'none'",
-      "base-uri 'self'",
-      "form-action 'self'",
-    ].join("; ")
+    "script-src 'self' 'unsafe-eval'; object-src 'none';"
   );
   next();
 });
