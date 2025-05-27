e1c253863ea22bf0740d32f02731679f0a38b9d3
DevArtist <53369947+DevArtist254@users.noreply.github.com>
2025-05-27 08:22:51 +0300

    Update DetailsView.vue

--- client/src/views/DetailsView.vue
+++ client/src/views/DetailsView.vue
@@ -40,7 +40,7 @@ export default {
           "http://localhost:3000";
 
         const res = await fetch(
-          `http://localhost:3000/app/v1/product/car_listing/${props.id}`
+          `http://eo00wo08gs08kwosoowksksg.devartist.art/app/v1/product/car_listing/${props.id}`
         );
 
         const message = await res.json();
